ce6a5b5384b13018a1baedbf1b03920ab7204666
murat <murat.yarali@outlook.com>
2022-04-04 04:28:54 +0300

    first tf file added

---
+++ workflow/learn-terraform-aws-instance/example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+ access_key = "Your AWS account's access_key provided by credentials_aws.csv"
+ secret_key = "Your AWS account's secret_key provided by credentials_aws.csv"
+ profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
