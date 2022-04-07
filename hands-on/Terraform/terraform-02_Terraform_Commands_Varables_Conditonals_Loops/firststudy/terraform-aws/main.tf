e595717f02b7977734f493b779caaaf7195e309e
murat <murat.yarali@outlook.com>
2022-04-08 00:14:49 +0300

    last commit about this hands on

--- hands-on/Terraform/terraform-02_Terraform_Commands_Varables_Conditonals_Loops/firststudy/terraform-aws/main.tf
+++ hands-on/Terraform/terraform-02_Terraform_Commands_Varables_Conditonals_Loops/firststudy/terraform-aws/main.tf
@@ -11,15 +11,22 @@ terraform {
   }
 }
 
+locals {
+    mytag = "oliver-local-name"
+}
+
 resource "aws_instance" "tf-ec2" {
   ami           = var.ec2_ami
   instance_type = var.ec2_type
   key_name      = "mk"
   tags = {
-    Name = "${var.ec2_name}-instance"
+    Name = "${local.mytag}-come from locals"
   }
 }
 
 resource "aws_s3_bucket" "tf-s3" {
   bucket = var.s3_bucket_name
+  tags= {
+    Name = "${local.mytag}-come from locals"
+  }
 }
