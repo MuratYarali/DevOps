987a84bf2c54ff377d5db99b3d302e12dd4d9f7d
murat <murat.yarali@outlook.com>
2022-04-17 12:08:40 +0300

    update DevOps Repo

--- projects/202-Terraform-Phonebook-Application-deployed-on-AWS/class-work/tf-files/user-data.sh
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/class-work/tf-files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/timothy-clark/phonebook.git
+TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+cd /home/ec2-user && git clone https://$TOKEN@github.com/MuratYarali/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
