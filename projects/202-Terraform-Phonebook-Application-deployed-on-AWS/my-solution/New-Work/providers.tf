db0dd167a2f1eefd9b558c756b207f5faee2c348
murat <murat.yarali@outlook.com>
2022-04-18 16:57:11 +0300

    files updated

---
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/my-solution/New-Work/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "github" {
+  token = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
