17d17681c2d87d12106927f663a36d5a120258ae
murat <murat.yarali@outlook.com>
2022-04-20 17:19:05 +0300

    upload the .tf file

--- hands-on/Docker/docker-01-installing-on-ec2-linux2/my-workflow/ec2-docker_new.tf
+++ hands-on/Docker/docker-01-installing-on-ec2-linux2/my-workflow/ec2-docker_new.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "tf-ec2" {
   ami                    = "ami-087c17d1fe0178315"
   instance_type          = var.instance-type
   key_name               = "mk"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [data.aws_security_group.allow_ssh.id]
   #  iam_instance_profile = "terraform"
   tags = {
     Name = "Docker-engine"
@@ -44,6 +44,8 @@ resource "aws_instance" "tf-ec2" {
               curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
               -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
+              yum install git -y
+              hostnamectl set-hostname "docker-compose-server"
 	            EOF
 }
 output "myec2-public-ip" {
