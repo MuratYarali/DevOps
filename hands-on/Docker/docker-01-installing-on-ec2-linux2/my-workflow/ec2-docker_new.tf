75fd5acd5769a9e865840c6fd5ba92e8e893c142
murat <murat.yarali@outlook.com>
2022-04-21 17:33:46 +0300

    update

--- hands-on/Docker/docker-01-installing-on-ec2-linux2/my-workflow/ec2-docker_new.tf
+++ hands-on/Docker/docker-01-installing-on-ec2-linux2/my-workflow/ec2-docker_new.tf
@@ -8,9 +8,8 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 
 variable "instance-type" {
@@ -19,10 +18,10 @@ variable "instance-type" {
 }
 
 data "aws_security_group" "allow_ssh" {
-  id = "sg-08b39f00f999db381"
+  id = "sg-000527cc1f9b6a3fe"
 }
 
-resource "aws_instance" "tf-ec2" {
+resource "aws_instance" "murat-terraform-ec2" {
   ami                    = "ami-087c17d1fe0178315"
   instance_type          = var.instance-type
   key_name               = "mk"
@@ -44,10 +43,9 @@ resource "aws_instance" "tf-ec2" {
               curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
               -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
-              yum install git -y
-              hostnamectl set-hostname "docker-compose-server"
 	            EOF
 }
+
 output "myec2-public-ip" {
-  value = aws_instance.tf-ec2.public_ip
+  value = aws_instance.murat-terraform-ec2.public_ip
 }
