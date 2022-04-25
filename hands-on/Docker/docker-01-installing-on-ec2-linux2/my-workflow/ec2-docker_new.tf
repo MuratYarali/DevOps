4894904ccdb464f8340f1f488e520e84fdd5f967
murat <murat.yarali@outlook.com>
2022-04-25 13:34:16 +0300

    Hands-on2 files added

--- hands-on/Docker/docker-01-installing-on-ec2-linux2/my-workflow/ec2-docker_new.tf
+++ hands-on/Docker/docker-01-installing-on-ec2-linux2/my-workflow/ec2-docker_new.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "murat-terraform-ec2" {
               amazon-linux-extras install docker -y
               systemctl start docker
               systemctl enable docker
-              usermod -a -G docker ec2-user
+              usermod -a -G docker $USER
               newgrp docker
               # install docker-compose
               curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
